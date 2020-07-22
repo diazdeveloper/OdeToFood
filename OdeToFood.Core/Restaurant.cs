58db7a864957fa6f21eabbf9134d2e2cb7106843
diazdeveloper <diazdeveloper@gmail.com>
2020-07-21 23:19:54 -0700

    Added annotations to restaurant class file

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -10,10 +10,12 @@ namespace OdeToFood.Core
 
         [Required]
         [StringLength(25)]
+        [Display(Name = "Restaurant Name")]
         public string Name { get; set; }
 
         [Required]
         [StringLength(25)]
+        [Display(Name = "Location (City)")]
         public string Location { get; set; }
 
         public CuisineType Cuisine { get; set; }
